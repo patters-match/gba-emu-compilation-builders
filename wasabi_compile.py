a3006833ca6ea414bdf0d0f4ba74a5350b1da6c0
patters-syno <55089098+patters-syno@users.noreply.github.com>
2022-10-02 23:38:17 +0100

    Update wasabi_compile.py

--- wasabi_compile.py
+++ wasabi_compile.py
@@ -38,8 +38,8 @@ def writefile(name, contents):
 #def get_bit(value, n):
 #    return ((value >> n & 1) != 0)
 
-def set_bit(value, n):
-    return value | (1 << n)
+#def set_bit(value, n):
+#    return value | (1 << n)
 
 #def clear_bit(value, n):
 #    return value & ~(1 << n)
